d70ceb1ffd99d95ca9a83607dd3afbccfb9d3af3
Brent Nycum <brentnycum@gmail.com>
2014-03-14 21:47:03 -0500

    Documentation

--- Garage/BNGarageApiClient.h
+++ Garage/BNGarageApiClient.h
@@ -12,11 +12,35 @@ extern NSString * const BNGarageApiHostURL;
 
 @interface BNGarageApiClient : AFHTTPClient
 
+/**
+ *  Shared Garage API client.
+ *
+ *  @return shared client.
+ */
 + (BNGarageApiClient *)sharedClient;
 
+/**
+ *  Switch the left garage door.
+ *
+ *  @param success success block
+ *  @param failure failure block
+ */
 - (void)switchLeftGarageWithSuccess:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
+
+/**
+ *  Switch the right garage door.
+ *
+ *  @param success success block
+ *  @param failure failure block
+ */
 - (void)switchRightGarageWithSuccess:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
 
+/**
+ *  Switch both garage doors.
+ *
+ *  @param success success block
+ *  @param failure failure block
+ */
 - (void)switchBothGaragesWithSuccess:(void (^)(id responseObject))success failure:(void (^)(NSError *error))failure;
 
 @end
